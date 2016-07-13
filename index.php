e432f01be43fff01dd6f5b99e0bdfea462784856
Alan Stines <alan.stines@mga.edu>
2016-07-13 19:59:41 -0400

    1st push

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello World</title>
+	<link href="style.css" rel="stylesheet" />
+</head>
+<body>
+	<h1><?php echo "hello world"; ?></h1>
+	TEst
+	<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+</body>
+</html>
