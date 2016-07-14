6eee558a3375261458a674a5e957edb3fe20e748
Alan Stines <alan.stines@mga.edu>
2016-07-13 20:24:59 -0400

    fixing junk

--- app/index.php
+++ app/index.php
@@ -2,11 +2,54 @@
 <html>
 <head>
 	<title>Hello World</title>
-	<link href="style.css" rel="stylesheet" />
+	<link href="_bower/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+	<script src="_bower/jquery/dist/jquery.min.js"></script>
+	<script src="_bower/bootstrap/dist/js/bootstrap.min.js"></script>
+	<link href="_src/style.css" rel="stylesheet" />
 </head>
 <body>
-	<h1><?php echo "hello world"; ?></h1>
-	TEst
+	<div class="container">
+		<header>
+			<h1><?php echo "hello world"; ?></h1>
+		</header>
+		<nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Project name</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li role="separator" class="divider"></li>
+                  <li class="dropdown-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="./">Default <span class="sr-only">(current)</span></a></li>
+              <li><a href="../navbar-static-top/">Static top</a></li>
+              <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
+	</div><!-- END CONTAINER -->
 	<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 </body>
 </html>
